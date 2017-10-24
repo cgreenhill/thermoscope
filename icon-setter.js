267a03d6c6888ba3b4b7bd2e026391e1235580a2
Colin Greenhill <clgreenhill@gmail.com>
2017-10-24 18:05:24 -0400

    Update gh-pages

--- icon-setter.js
+++ icon-setter.js
@@ -188,7 +188,7 @@
 	        console.log("current icon value: " + iconVal);
 
 	        console.log("writing value: 💡");
-	        return characteristic.writeValue(component.encoder.encode("💡"));
+	        return component.iconCharacteristic.writeValue(component.encoder.encode("💡"));
 	      }).catch(function (error) {
 	        console.error('Connection failed!', error);
 	        component.setState({
